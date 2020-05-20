8cf955734f73bf401911bc5aa06c2c70225b8b33
S.Tanaka <5511068t@gmail.com>
2020-05-20 20:53:20 +0900

    田中が記載した課題

---
+++ tanaka_Task.php
@@ -0,0 +1,34 @@
+<?php
+    echo "課題1\n";
+    $a = 3;
+    $b = 7;
+    echo $a + $b . "\n";
+    echo "課題2\n";
+    $array_month = ["1月","2月","3月","4月","5月","6月",
+                    "7月","8月","9月","10月","11月","12月"];
+    echo $array_month[7]."\n";
+    echo "課題3\n";
+    $hello = "Hello,";
+    $name  = "tanaka";
+    $world = "'s World!\n";
+    echo $hello . $name . $world;
+    echo "課題4\n";
+    $tech_boost = "tech ";
+    $tech_boost .= "boost";
+    echo $tech_boost . "\n";
+    echo "課題5\n";
+    $calendar = [
+        "January" => "1月",
+        "February" => "2月",
+        "March" => "3月",
+        "April" => "4月",
+        "May" => "5月",
+        "June" => "6月",
+        "July" => "7月",
+        "August" => "8月",
+        "September" => "9月",
+        "October" => "10月",
+        "November" => "11月",
+        "December" => "12月"
+    ];
+    echo $calendar[December] . "\n";
